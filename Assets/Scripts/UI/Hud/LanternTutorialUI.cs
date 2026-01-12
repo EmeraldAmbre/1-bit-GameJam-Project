33a79794ed7799cb7e457a4ea06619969a0dab4b
Ambre <bourdon_ambre@outlook.fr>
2026-01-12 15:02:34 +0100

    Update LanternTutorialUI.cs

--- Assets/Scripts/UI/Hud/LanternTutorialUI.cs
+++ Assets/Scripts/UI/Hud/LanternTutorialUI.cs
@@ -1,9 +1,20 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LanternTutorialUI : MonoBehaviour
 {
+    [Header("Tutorial Settings")]
+    [SerializeField] private int _showOnlyInSceneIndex = 0; // scène 1 = index 0
     private void Awake()
     {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentSceneIndex != _showOnlyInSceneIndex)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+
         Time.timeScale = 0f;
     }
 
